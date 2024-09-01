899b25ad0bb66cec794acb44ec39fbbd8979066a
vagrant <vagrant@terraform-box>
2024-09-01 05:36:47 +0000

    vault dyanmic role

---
+++ tfc-create-vault-aws-secret-dynamic-role/data.tf
@@ -0,0 +1,16 @@
+data "terraform_remote_state" "vault_admin" {
+  backend = "remote"
+
+  config = {
+    organization = "hc-tfc"
+    workspaces = {
+      name = "aws-iam-creation"
+    }
+  }
+}
+
+
+data "vault_aws_access_credentials" "master_netadmin_creds" {
+  backend = vault_aws_secret_backend.aws.path
+  role    = vault_aws_secret_backend_role.dynamic_role.name
+}
