f1f5a9374b4f5d97d99f71f2e5cedbe17549a03e
vagrant <vagrant@terraform-box>
2024-09-04 14:42:51 +0000

    Modified Vault Secret Backend Role

--- tfc-create-vault-aws-secret-dynamic-role/output.tf
+++ tfc-create-vault-aws-secret-dynamic-role/output.tf
@@ -1,20 +1,20 @@
 output "dynamic_role" {
   description = "Read the Master NetworkAdmin Role"
-  value = resource.vault_aws_secret_backend_role.dynamic_role
+  value       = resource.vault_aws_secret_backend_role.dynamic_role
 }
 
 output "IAM_User_IAMAdmin_Role" {
   description = "Output of IAM User Master NetworkAdmin Role"
-  value = data.vault_aws_access_credentials.master_netadmin_creds
-  sensitive = true
+  value       = data.vault_aws_access_credentials.master_netadmin_creds
+  sensitive   = true
 }
 
 output "backend_path" {
   description = "Output of Vault Secret Backend Path"
-  value = vault_aws_secret_backend.aws.path
+  value       = vault_aws_secret_backend.aws.path
 }
 
 output "backend_role" {
   description = "Output of Vault Secret Backend Role Name"
-  value = vault_aws_secret_backend_role.dynamic_role.name
+  value       = vault_aws_secret_backend_role.dynamic_role.name
 }
