77b9defe884c09d0d3650e4dfbf66b5e4bb5171c
vagrant <vagrant@terraform-box>
2024-09-12 14:22:29 +0000

    Modify tf file

--- create-ec2-rds/output.tf
+++ create-ec2-rds/output.tf
@@ -1,30 +1,30 @@
 output "rds_hostname" {
   description = "RDS instance hostname"
-  value       = aws_db_instance.project_rds.address
+  value       = aws_db_instance.db.address
   sensitive   = true
 }
 output "rds_port" {
   description = "RDS instance port"
-  value       = aws_db_instance.project_rds.port
+  value       = aws_db_instance.db.port
   sensitive   = true
 }
 output "rds_username" {
   description = "RDS instance root username"
-  value       = aws_db_instance.project_rds.username
+  value       = aws_db_instance.db.username
   sensitive   = true
 }
 output "endpoints" {
   value = <<EOF
-AWS RDS Endpoint:  ${aws_db_instance.project_rds.endpoint}
+AWS RDS Endpoint:  ${aws_db_instance.db.endpoint}
 For example:
-    mysql -h ${aws_db_instance.project_rds.id} -P ${aws_db_instance.project_rds.port} -u ${aws_db_instance.project_rds.username} -p
+    mysql -h ${aws_db_instance.db.id} -P ${aws_db_instance.db.port} -u ${aws_db_instance.db.username} -p
 Jump Server IP (public):  ${aws_instance.jump.public_ip}
 Jump Server IP (private): ${aws_instance.jump.private_ip}
 For example:
    ssh -i ${aws_key_pair.main.key_name}.pem ubuntu@${aws_instance.jump.public_ip}
-APP Client IP (private): ${aws_instance.app.private_ip}
+APP Client IP (private): ${aws_instance.application.private_ip}
 For example:
-   ssh -i ${aws_key_pair.main.key_name}.pem ubuntu@${aws_instance.app.private_ip}
+   ssh -i ${aws_key_pair.main.key_name}.pem ubuntu@${aws_instance.application.private_ip}
 APP Client IAM Role ARN: ${data.aws_iam_role.vault-client.arn}
 EOF
 }
