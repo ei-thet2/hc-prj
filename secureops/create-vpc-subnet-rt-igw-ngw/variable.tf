bea75280db6c9771406416732a09a10bbf0b17aa
vagrant <vagrant@terraform-box>
2024-09-24 13:55:16 +0000

    vpc-public-subnet

---
+++ secureops/create-vpc-subnet-rt-igw-ngw/variable.tf
@@ -0,0 +1,33 @@
+variable "public_subnet_01" {
+  default = "10.10.1.0/24"
+  description = "public-subnet-01"
+}
+
+variable "az_1a" {
+  type = string
+  default = "ap-southeast-1a"
+  description = "availability_zone"
+}
+
+variable "public_subnet_02" {
+  default = "10.10.1.0/24"
+  description = "public-subnet-02"
+}
+
+variable "az_1b" {
+  type = string
+  default = "ap-southeast-1b"
+  description = "availability_zone"
+}
+
+variable "public_subnet_03" {
+  default = "10.10.1.0/24"
+  description = "public-subnet-03"
+}
+
+variable "az_1c" {
+  type = string
+  default = "ap-southeast-1c"
+  description = "availability_zone"
+}
+
