977961f83ff9fd93d0b8a3a4d1e9d19b2ed3c598
vagrant <vagrant@terraform-box>
2024-09-26 14:25:12 +0000

    private-db-subnets

--- secureops/create-vpc-subnet-rt-igw-ngw/variable.tf
+++ secureops/create-vpc-subnet-rt-igw-ngw/variable.tf
@@ -35,3 +35,13 @@ variable "public_subnets" {
   default = ["10.10.1.0/24","10.10.2.0/24","10.10.3.0/24"]
   description = "public-subnets"
 }
+
+variable "private_subnets" {
+  default = ["10.10.4.0/24","10.10.5.0/24","10.10.6.0/24"]
+  description = "private-subnets"
+}
+
+variable "db_subnets" {
+  default = ["10.10.7.0/24","10.10.8.0/24","10.10.9.0/24"]
+  description = "db-subnets"
+}
